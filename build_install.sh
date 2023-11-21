7ba44d4502d261e69c19183facfff30ff93e2feb
Nikos Avgeros <nikosavgeros@Nikoss-MBP.lan>
2023-11-21 16:43:35 +0200

    Create bash file to install timepulse using pip

---
+++ build_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update the repository
+git pull
+
+# Remove the 'dist' and 'build' folders
+rm -rf dist build timepulse.egg-info
+
+# Build source and wheel distributions using setup.py
+python setup.py sdist bdist_wheel
+
+# Uninstall the existing 'timepulse'
+pip uninstall -y timepulse
+
+# Install the updated version of 'timepulse'
+pip install .
