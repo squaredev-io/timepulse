e762d42d3056eb9ad68ca2bee80622c98755006a
iamtheef <eftihis.riise@gmail.com>
2023-11-22 15:18:10 +0200

    Ommited fetch weather api functionality & added local files

--- build_install.sh
+++ build_install.sh
@@ -1,43 +1,6 @@
 #!/bin/bash
 
-# Specify the repository URL
-repo_url="https://github.com/stavrostheocharis/weather_data_retriever.git"
-
-# Specify the folder name
-folder_name="weather_data_retriever"
-
-# Specify the package name
-package_name="weather_data_retriever"
-
-# Create the folder
-mkdir -p $folder_name
-
-# Clone the repository into the folder
-git clone $repo_url $folder_name
-
-# Optional: Navigate into the cloned folder
-cd $folder_name
-
-# Remove existing build artifacts
-rm -rf dist build $package_name.egg-info
-
-# Build source and wheel distributions using setup.py
-python setup.py sdist bdist_wheel
-
-# Uninstall the existing package
-pip uninstall -y $package_name
-
-# Install the updated version of the package
-pip install .
-
-# Navigate back to the original directory
-cd ..
-
-# Remove the cloned folder
-rm -rf $folder_name
-
-# Update the repository
-git pull
+pip install git+https://github.com/stavrostheocharis/weather_data_retriever.git
 
 # Remove the 'dist' and 'build' folders
 rm -rf dist build timepulse.egg-info
