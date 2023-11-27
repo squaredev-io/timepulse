fix the bug in predict function for scaling and add typing to parameters

timepulse/models/nbeats.py
@@ -1,41 +1,81 @@
 import tensorflow as tf
-from datetime import datetime
+import numpy as np
 from timepulse.utils.models import create_early_stopping
+from typing import Tuple, List, Dict
 
-# Create NBeatsBlock custom layer
 class NBeatsBlock(tf.keras.layers.Layer):
+    """
+    Custom layer for the N-Beats model, consisting of stacked fully connected layers with ReLU activation,
+    followed by a theta layer with linear activation.
+
+    Parameters
+    ----------
+    input_size : int
+        The size of the input for the block.
+
+    theta_size : int
+        The size of the theta layer.
+
+    horizon : int
+        The forecasting horizon.
+
+    n_neurons : int
+        The number of neurons in each hidden layer.
+
+    n_layers : int
+        The number of hidden layers in the block.
+
+    **kwargs
+        Additional keyword arguments for the parent class (input_shape, trainable, name).
+
+    Returns
+    -------
+    backcast : np.array
+        The backcast predictions.
+
+    forecast : np.array
+        The forecast predictions.
+    """
     def __init__(
-        self,  # the constructor takes all the hyperparameters for the layer
+        self, 
         input_size: int,
         theta_size: int,
         horizon: int,
         n_neurons: int,
         n_layers: int,
-        **kwargs,
-    ):  # the **kwargs argument takes care of all of the arguments for the parent class (input_shape, trainable, name)
+        **kwargs: Dict,
+    ) -> None:  
         super().__init__(**kwargs)
         self.input_size = input_size
         self.theta_size = theta_size
         self.horizon = horizon
         self.n_neurons = n_neurons
         self.n_layers = n_layers
-
-        # Block contains stack of 4 fully connected layers each has ReLU activation
         self.hidden = [tf.keras.layers.Dense(n_neurons, activation="relu") for _ in range(n_layers)]
-        # Output of block is a theta layer with linear activation
         self.theta_layer = tf.keras.layers.Dense(theta_size, activation="linear", name="theta")
 
-    def call(self, inputs):  # the call method is what runs when the layer is called
+    def call(self, inputs: np.array) -> Tuple[np.array, np.array]:
+        """
+        Forward pass through the NBeatsBlock.
+
+        Parameters
+        ----------
+        inputs : np.array
+            The input data.
+
+        Returns
+        -------
+        Tuple[np.array, np.array]
+            Containing the backcast and forecast predictions.
+        """
         x = inputs
-        for layer in self.hidden:  # pass inputs through each hidden layer
+        for layer in self.hidden:
             x = layer(x)
         theta = self.theta_layer(x)
-        # Output the backcast and forecast from theta
         backcast, forecast = theta[:, : self.input_size], theta[:, -self.horizon :]
         return backcast, forecast
 
 
-# Custom NBeats model
 class NBeats(tf.keras.Model):
     """
     N-Beats Model for time series forecasting.
@@ -57,6 +97,18 @@ class NBeats(tf.keras.Model):
     n_stacks : int, optional, default: 30
         The number of stacked NBeatsBlocks in the model.
 
+    epochs : int, optional, default: 5000
+        The number of training epochs.
+
+    batch_size : int, optional, default: 1024
+        The batch size for training.
+
+    callbacks : List[tf.keras.callbacks.Callback], optional, default: [ReduceLROnPlateau, EarlyStopping]
+        List of callbacks to monitor and control the training process.
+
+    **kwargs
+        Additional keyword arguments for the parent class.
+
     Attributes
     ----------
     model : tf.keras.Model
@@ -70,7 +122,7 @@ class NBeats(tf.keras.Model):
     compile(loss="mae", learning_rate=0.001, metrics=["mae", "mse"])
         Compile the NBeats model with specified loss function, learning rate, and metrics.
 
-    fit(X_train, y_train, X_val, y_val, n_epochs=5000, batch_size=1024, verbose=0)
+    fit(X_train, y_train, X_val, y_val, verbose=0)
         Train the NBeats model with EarlyStopping and ReduceLROnPlateau callbacks.
 
     predict(X_test)
@@ -85,22 +137,21 @@ class NBeats(tf.keras.Model):
     >>> predictions = nbeats_instance.predict(test_data)
     >>> mse = nbeats_model.model.evaluate(test_data)
     """
-
     def __init__(
         self, 
-        window_size,
-        horizon=1,
-        n_neurons=512,
-        n_layers=4, 
-        n_stacks=30, 
-        epochs=5000, 
-        batch_size=1024, 
-        callbacks=[
+        window_size: int,
+        horizon: int = 1,
+        n_neurons: int = 512,
+        n_layers: int = 4, 
+        n_stacks: int = 30, 
+        epochs: int = 5000, 
+        batch_size: int = 1024, 
+        callbacks: List = [
                 tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=100, verbose=1),
                 create_early_stopping(monitor="val_loss", patience=200, restore_best_weights=True),
             ],
-        **kwargs
-    ):
+        **kwargs: Dict
+    ) -> None:
         super().__init__(**kwargs)
         self.input_size = window_size * horizon
         self.theta_size = window_size * horizon + horizon
@@ -114,8 +165,6 @@ class NBeats(tf.keras.Model):
         self.model_name = f"nbeats_model"
         self.model = None
         self.callbacks = callbacks
-
-        # Create the initial NBeatsBlock layer
         self.initial_block = NBeatsBlock(
             input_size=window_size * horizon,
             theta_size=window_size * horizon + horizon,
@@ -125,7 +174,7 @@ class NBeats(tf.keras.Model):
             name="InitialBlock",
         )
 
-    def build(self):
+    def build(self) -> None:
         stack_input = tf.keras.layers.Input(shape=(self.input_size), name="stack_input")
         backcast, forecast = self.initial_block(stack_input)
         residuals = tf.keras.layers.subtract([stack_input, backcast], name="subtract_00")
@@ -142,10 +191,10 @@ class NBeats(tf.keras.Model):
             forecast = tf.keras.layers.add([forecast, block_forecast], name=f"add_{i}")
         self.model = tf.keras.Model(inputs=stack_input, outputs=forecast, name=self.model_name)
 
-    def compile(self, loss="mae", learning_rate=0.001, metrics=["mae", "mse"]):
+    def compile(self, loss: str = "mae", learning_rate: float = 0.001, metrics: List = ["mae", "mse"]) -> None:
         self.model.compile(loss=loss, optimizer=tf.keras.optimizers.legacy.Adam(learning_rate), metrics=metrics)
 
-    def fit(self, X_train, y_train, X_val, y_val, verbose=0):
+    def fit(self, X_train: np.array, y_train: np.array, X_val: np.array, y_val: np.array, verbose: int = 0) -> None:
         self.model.fit(
             X_train,
             y_train,
@@ -156,6 +205,6 @@ class NBeats(tf.keras.Model):
             callbacks=self.callbacks,
         )
 
-    def predict(self, X_test):
+    def predict(self, X_test: np.array) -> np.array:
         y_pred = self.model.predict(X_test)
         return y_pred.flatten()

timepulse/models/xgboost.py
@@ -1,24 +1,25 @@
 from xgboost import XGBRegressor
 from timepulse.processing.min_max_scaler import MinMaxScalerWrapper
-from datetime import datetime
+import numpy as np
 import joblib
 import os
+from typing import Type, Dict
 
 class XGBRegressorModel:
-    def __init__(self, scaler_class=MinMaxScalerWrapper(), **kwargs):
+    def __init__(self, scaler_class: Type = MinMaxScalerWrapper(), **kwargs: Dict) -> None:
         self.params = kwargs
         self.scaler_X = scaler_class
         self.scaler_y = scaler_class
         self.model_name = f"xgboost_model"
         self.model = None
         
-    def build(self):
+    def build(self) -> None:
         if self.params:
             self.model = XGBRegressor()
         else:
             self.model = XGBRegressor(**self.params)
 
-    def fit(self, X_train, y_train, X_val, y_val, verbose=0):
+    def fit(self, X_train: np.array, y_train: np.array, X_val: np.array, y_val: np.array, verbose: int = 0) -> None:
         if self.scaler_X is not None:
             X_train = self.scaler_X.fit_transform_X(X_train)
             y_train = self.scaler_y.fit_transform_y(y_train.reshape(len(y_train), 1)).flatten()
@@ -26,16 +27,16 @@ class XGBRegressorModel:
             y_val = self.scaler_y.transform_y(y_val.reshape(len(y_val), 1)).flatten()
         self.model.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_val, y_val)], verbose=verbose)
 
-    def predict(self, X_test):
+    def predict(self, X_test: np.array) -> None:
         if self.scaler_X is not None:
-            X_test = self.scaler_X.fit_transform_X(X_test)
+            X_test = self.scaler_X.transform_X(X_test)
             y_pred = self.model.predict(X_test)
             y_pred = self.scaler_y.inverse_transform_y(y_pred.reshape(-1,1))
         else:
             y_pred = self.model.predict(X_test)
         return y_pred.flatten()
     
-    def save(self, save_path='storage'):
+    def save(self, save_path: str = 'storage') -> None:
         model_filename = f'{save_path}/{self.model_name}'
         if not os.path.exists(model_filename):
             os.makedirs(model_filename)

timepulse/processing/standard_scaler.py
@@ -1,24 +1,25 @@
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 class StandardScalerWrapper:
-    def __init__(self):
+    def __init__(self) -> None:
         self.scaler_X = StandardScaler()
         self.scaler_y = StandardScaler()
 
-    def fit_transform_X(self, data):
+    def fit_transform_X(self, data: np.array) -> np.array:
         return self.scaler_X.fit_transform(data)
 
-    def transform_X(self, data):
+    def transform_X(self, data: np.array) -> np.array:
         return self.scaler_X.transform(data)
 
-    def inverse_transform_X(self, data):
+    def inverse_transform_X(self, data: np.array) -> np.array:
         return self.scaler_X.inverse_transform(data)
 
-    def fit_transform_y(self, data):
+    def fit_transform_y(self, data: np.array) -> np.array:
         return self.scaler_y.fit_transform(data)
 
-    def transform_y(self, data):
+    def transform_y(self, data: np.array) -> np.array:
         return self.scaler_y.transform(data)
 
-    def inverse_transform_y(self, data):
+    def inverse_transform_y(self, data: np.array) -> np.array:
         return self.scaler_y.inverse_transform(data)

timepulse/utils/models.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import os
+import numpy as np
+from typing import Tuple, Dict, Type
 from timepulse.metrics.regression_metrics import evaluate_preds
 
-def create_model_checkpoint(model_name, save_path='storage'):
+def create_model_checkpoint(model_name: str, save_path: str = 'storage') -> tf.keras.callbacks.ModelCheckpoint:
     """
     Creates a ModelCheckpoint callback for saving the best model during training.
 
@@ -24,14 +26,14 @@ def create_model_checkpoint(model_name, save_path='storage'):
                                             save_best_only=True)
 
 
-def create_early_stopping(monitor='val_loss', patience=50, restore_best_weights=True):
+def create_early_stopping(monitor: str = 'val_loss', patience: int = 50, restore_best_weights: bool = True) -> tf.keras.callbacks.EarlyStopping:
     """
     Create an EarlyStopping callback for a Keras model.
 
     Parameters
     ----------
     monitor : str, optional
-        Quantity to be monitored (default is 'vali_loss').
+        Quantity to be monitored (default is 'val_loss').
     patience : int, optional
         Number of epochs with no improvement after which training will be stopped (default is 50).
     restore_best_weights : bool, optional
@@ -49,35 +51,33 @@ def create_early_stopping(monitor='val_loss', patience=50, restore_best_weights=
     return tf.keras.callbacks.EarlyStopping(monitor=monitor, patience=patience, restore_best_weights=restore_best_weights)
 
 
-def run_model(model_instance, X_train, y_train, X_test, y_test, verbose=0):
+def run_model(model_instance: Type, X_train: np.array, y_train: np.array, X_test: np.array, y_test: np.array, verbose=0) -> Tuple[np.array, Dict]:
     """
     Train and evaluate a Keras model.
 
     Parameters
     ----------
-    model_instance : tf.keras.Model
-        An instance of a Keras model.
-    X_train : Union[tf.Tensor, tf.data.Dataset]
+    model_instance : Type
+        An instance of any class.
+    X_train : np.array
         Input features for training.
-    y_train : Union[tf.Tensor, tf.data.Dataset]
+    y_train : np.array
         Target labels for training.
-    X_test : Union[tf.Tensor, tf.data.Dataset]
+    X_test : np.array
         Input features for testing.
-    y_test : Union[tf.Tensor, tf.data.Dataset]
+    y_test : np.array
         Target labels for testing.
-    threshold : float, optional
-        Threshold for binary classification (default is 0.75).
     verbose : int, optional
         Verbosity mode (default is 0).
 
     Returns
     -------
-    Tuple[tf.Tensor, dict]
+    Tuple[np.array, dict]
         Tuple containing the model predictions and evaluation metrics.
 
     Example
     -------
-    y_pred, result_metrics = run_model(model_instance, X_train, y_train, X_test, y_test, threshold=0.8, verbose=1)
+    y_pred, result_metrics = run_model(model_instance, X_train, y_train, X_test, y_test, verbose=0)
     """
     model_instance.build()
     if hasattr(model_instance, 'compile'):

timepulse/utils/splits.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
+from typing import Union, Tuple
 
 
-def create_multivar_dataframe(base_df, *additional_dfs):
+def create_multivar_dataframe(base_df: pd.DataFrame, *additional_dfs: pd.DataFrame) -> pd.DataFrame:
     """
     Create a multivariate DataFrame by merging a base DataFrame with additional DataFrames based on a date range.
 
@@ -43,7 +44,7 @@ def create_multivar_dataframe(base_df, *additional_dfs):
     return multivar_df
 
 
-def create_windowed_dataframe(base_df, target_column, window_size=3):
+def create_windowed_dataframe(base_df: pd.DataFrame, target_column: str, window_size: int = 3) -> pd.DataFrame:
     """
     Create a windowed DataFrame by shifting values of a specified column.
 
@@ -72,7 +73,7 @@ def create_windowed_dataframe(base_df, target_column, window_size=3):
     return windowed_df
 
 
-def stratified_split_data(data, target_column, splitter_column, test_size=0.2, random_state=42, n_splits=1, strat_split_index=0):
+def stratified_split_data(data: pd.DataFrame, target_column: str, splitter_column: str, test_size: float = 0.2, random_state: int = 42, n_splits: int = 1, strat_split_index: int = 0) -> Union[pd.DataFrame, np.array, pd.DataFrame, np.array]:
     """
     Perform stratified splitting of data into train and test sets.
 
@@ -115,7 +116,7 @@ def stratified_split_data(data, target_column, splitter_column, test_size=0.2, r
     return X_train, y_train, X_test, y_test
 
 
-def get_labelled_windows(x, horizon=1):
+def get_labelled_windows(x, horizon: int = 1) -> Tuple[np.array, np.array]:
     """
     Creates labels for windowed dataset.
 
@@ -125,7 +126,7 @@ def get_labelled_windows(x, horizon=1):
     return x[:, :-horizon], x[:, -horizon:]
 
 
-def make_windows(x, window_size=7, horizon=1):
+def make_windows(x, window_size: int = 7, horizon: int = 1) -> Tuple[np.array, np.array]:
     """
     Create function to view NumPy arrays as windows. 
     Turns a 1D array into a 2D array of sequential windows of window_size.
@@ -145,11 +146,11 @@ def make_windows(x, window_size=7, horizon=1):
     return windows, labels
 
 
-def make_train_test_splits(windows, labels, test_split=0.1):
+def make_train_test_splits(windows: int, labels: int, test_split: float = 0.1):
     """
     Splits matching pairs of windows and labels into train and test splits.
     """
-    split_size = int(len(windows) * (1 - test_split))  # default: 90% train / 10% test
+    split_size = int(len(windows) * (1 - test_split))
     train_windows = windows[:split_size]
     train_labels = labels[:split_size]
     test_windows = windows[split_size:]
@@ -157,7 +158,7 @@ def make_train_test_splits(windows, labels, test_split=0.1):
     return train_windows, test_windows, train_labels, test_labels
 
 
-def make_window_splits(values, size=10, horizon=1):
+def make_window_splits(values: np.array, size:int = 10, horizon: int = 1):
     """
     Splits a time series into input windows and corresponding labels.
     """